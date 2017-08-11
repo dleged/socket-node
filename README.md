7178734d656c9f99eda9bab1cfdc48cd63f83274
faithZZZ <1208674494@qq.com>
2017-08-11 10:27:39 +0800

    提交: 完整源代码

---
+++ README.md
@@ -0,0 +1,19 @@
+/***服务器端***/
+使用socket.io -- listen(http的server)
+io.on('connection',function(socket));//监听客户端连接,回调函数会传递本次连接的socket
+io.sockets.emit('String',data);//给所有客户端广播消息
+io.sockets.socket(socketid).emit('String', data);//给指定的客户端发送消息
+socket.on('String',function(data));//监听客户端发送的信息
+socket.emit('String', data);//给该socket的客户端发送消息
+
+/***客户端***/
+建立一个socket与服务器连接
+var socket = io.connect('http://192.168.9.56:8000/');
+
+socket.on('msg',function(data){//监听服务消息
+    socket.emit('msg', {rp:"fine,thank you"}); //向服务器发送消息
+    console.log(data);
+});
+
+
+注意：socket 的任何事件如msg，服务器端的emit不会触发on的msg事件，客户端亦如此。
---
+++ public/javascripts/chat_ui.js
@@ -0,0 +1,95 @@
+/*用来显示系统创建的受信内容*/
+function divEscapedContentElement(message){
+	return $('<div></div>').text(message);
+}
+
+function divSystemContentElement(message){
+	return $('<div></div>').html('<i>' + message + '</i>');
+}
+
+/*处理原始的用户输入*/
+function processUserInput(chatApp, socket) {
+  var message = $('#send-message').val();
+  var systemMessage;
+
+  if (message.charAt(0) == '/') {
+		systemMessage = chatApp.processCommand(message);
+	if (systemMessage) {
+  		$('#messages').append(divSystemContentElement(systemMessage));
+    }
+  	} else {
+	    chatApp.sendMessage($('#room').text(), message);
+		$('#messages').append(divEscapedContentElement(message));
+		$('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  	}
+
+  $('#send-message').val('');
+}
+
+// socket连接到服务器
+var socket = io.connect('http://192.168.9.56:8000/');
+console.dir(socket);
+$(document).ready(function(){
+	var chatApp = new Chat(socket);
+	
+	//更名结果
+	socket.on('nameResult',function(result){
+		var message;
+		
+		if(result.success){
+			message = 'Your are now know as ' + result.name + '.';
+		}else{
+			message = 'result.message';
+		}
+		
+		$('#messages').append(divSystemContentElement(message));
+	});
+	
+	//显示房间变更结果
+	socket.on('joinResult', function(result) {
+	    $('#room').text(result.room);
+	    $('#messages').append(divSystemContentElement('Room changed.'));
+	});
+	
+	//显示接收到的消息
+	socket.on('message', function (message) {
+	    var newElement = $('<div></div>').text(message.text);
+	    $('#messages').append(newElement);
+    });
+	
+	//显示可用房间列表
+	socket.on('rooms',function(rooms){
+		$('#room-list').empty();
+		for(var room in rooms){
+			room = room.substring(1,room.length);
+			if(room != ''){
+				 $('#room-list').append(divEscapedContentElement(room));
+			}
+		}
+		//点击切换房间
+		$('#room-list div').click(function(){
+			chatApp.processCommand('/join ' + $(this).text());
+			$('#send-message').focus();
+			$('#messages').scrollTop($('#messages').prop('scrollHeight')+20);
+		});
+	});
+	//定期查看可用房间列表
+	setInterval(function(){
+		/*触发chat_server.js 的
+		socket.on('getRooms', function() {
+	      	socket.emit('rooms', io.sockets.manager.rooms);//
+	    });*/
+		socket.emit('getRooms');
+	},1000);
+	$('#send-message').focus();
+	
+	//提交表单发送消息
+	$('#send-form').submit(function(){
+		processUserInput(chatApp,socket);
+		return false;
+	});
+	socket.on('msg',function(data){
+			console.log(222);
+		});
+	
+});
